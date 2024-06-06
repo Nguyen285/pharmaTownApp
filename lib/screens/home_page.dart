62793eb2f6cf675023164a9746fd27759e7f3501
khangNguyen18 <minhkhang072003@gmail.com>
2024-06-06 14:10:57 +0700

    edit home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,59 +13,74 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ColorConstant.primaryColor,
-        toolbarHeight: 250,
-        actions: [
-          Column(
-            children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorConstant.buttonColor),
-                      onPressed: () {},
-                      child: const Expanded(
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.account_circle,
-                              color: Colors.white,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Đăng nhập/Đăng ký',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
+    return ListView(children: [
+      Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            color: ColorConstant.primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 25, 15, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorConstant.buttonColor),
+                        onPressed: () {},
+                        child: const Expanded(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.account_circle,
+                                color: Colors.white,
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                'Đăng nhập/Đăng ký',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.textsms_outlined,
-                        color: Colors.white,
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.textsms_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.shopping_cart_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.shopping_cart_outlined,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  Image.asset('assets/images/logo.png'),
+                ],
               ),
-              Image.asset('assets/images/logo.png'),
-            ],
+            ),
+          ),
+          Positioned(
+            width: 500,
+            bottom: -25,
+            child: Text("data"),
           ),
         ],
       ),
-    );
+    ]);
   }
 }
